c292360ae7c2fc94636896a62fa186eb02c01420
Dane Ahern <danielaherniv@gmail.com>
2019-10-25 23:45:52 -0400

    worked out some bugs

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,13 +1,13 @@
-//Select DOM Items
+// Select DOM Items
 
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
+const navItems = document.querySelectorAll('.nav-item');
 
-const navItems = document.querySelectorAll('.nav-item')
+// Set Initial State Of Menu
 
-//Set Initial State of Menu
 let showMenu = false;
 
 menuBtn.addEventListener('click', toggleMenu);
@@ -18,20 +18,20 @@ function toggleMenu() {
         menu.classList.add('show');
         menuNav.classList.add('show');
         menuBranding.classList.add('show');
-        navItems.forEach(item => item.classList.add('show'))
+        navItems.forEach(item => item.classList.add('show'));
 
-        //Set Menu State
+        // Set Menu State
+
         showMenu = true;
     } else {
         menuBtn.classList.remove('close');
         menu.classList.remove('show');
         menuNav.classList.remove('show');
         menuBranding.classList.remove('show');
-        navItems.forEach(item => item.classList.remove('show'))
+        navItems.forEach(item => item.classList.remove('show'));
 
-        //Set Menu State
+        // Set Menu State
+
         showMenu = false;
-
     }
 }
-
